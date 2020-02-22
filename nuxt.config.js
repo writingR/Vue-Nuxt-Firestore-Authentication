2d87b8b633be39fdf8b61f8174364936924dfeb3
writingr <writing_r@naver.com>
2020-02-22 15:56:43 +0900

    vue & nuxt setting and firestore connect

---
+++ nuxt.config.js
@@ -0,0 +1,107 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv',
+    [
+      'nuxt-fire',
+      {
+        config: {
+          apiKey: 'AIzaSyAeVrGLdsy0-OZSNyj2P4xt6kWB6tXHtus',
+          authDomain: 'nuxt-blog-t.firebaseapp.com',
+          databaseURL: 'https://nuxt-blog-t.firebaseio.com',
+          projectId: 'nuxt-blog-t',
+          storageBucket: 'nuxt-blog-t.appspot.com',
+          messagingSenderId: '57865596648',
+          appId: '1:57865596648:web:59dc76f1188da9766fe35d',
+          measurementId: 'G-P2069HGR23'
+        },
+        services: {
+          auth: true, // Just as example. Can be any other service.
+          firestore: true
+        }
+      }
+    ]
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container fluid>
+    <v-card>
+      {{ items }}
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      items: []
+    }
+  },
+  async mounted () {
+    const sn = await this.$fireStore.collection('test').get()
+    sn.docs.forEach((v) => {
+      // eslint-disable-next-line no-console
+      console.log(v.data())
+      this.items.push(v.data())
+    })
+    // console.log(sn)
+  }
+}
+</script>
