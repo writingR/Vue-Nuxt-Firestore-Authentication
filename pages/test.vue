e95f4417031913073ddd43f0a52740076a75bdd5
writingr <writing_r@naver.com>
2020-02-23 17:38:07 +0900

    firebase auth add

---
+++ pages/test.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container fluid>
+    <v-card>
+      <v-btn :loading="loading" @click="signIn">
+        signIn
+      </v-btn>
+      <v-btn :loading="loading" @click="signOut">
+        signOut
+      </v-btn>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+// import delay from 'delay'
+
+export default {
+  data () {
+    return {
+      loading: false
+    }
+  },
+  methods: {
+    async signIn () {
+      this.loading = true
+      try {
+        const provider = new this.$fireAuthObj.GoogleAuthProvider()
+        const user = await this.$fireAuth.signInWithPopup(provider)
+        console.log(user)
+      } catch (err) {
+        console.log('error', err);
+      } finally {
+        this.loading = false
+      }
+    },
+    async signOut () {
+      this.loading = true
+      try {
+        // eslint-disable-next-line no-unused-vars
+        const provider = new this.$fireAuthObj.GoogleAuthProvider()
+        // eslint-disable-next-line no-unused-vars
+        const user = await this.$fireAuth.signOut()
+        console.log(user);
+      } catch (err) {
+        console.log('error', err);
+      } finally {
+        this.loading = false
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- pages/write.vue
+++ pages/write.vue
@@ -1,25 +1,44 @@
 <template>
   <v-container fluid>
-    <v-card>
+    <v-card v-if="user">
       <v-toolbar dense color="success" dark flat>
-        <v-toolbar-title>write</v-toolbar-title>
+        <v-toolbar-title>{{ user.displayName }} write</v-toolbar-title>
         <v-spacer />
         <v-btn icon @click="save">
           <v-icon>mdi-content-save</v-icon>
         </v-btn>
+        <v-btn icon @click="signOut">
+          <v-icon>mdi-account-box</v-icon>
+        </v-btn>
       </v-toolbar>
       <v-form>
         <v-card-text>
-          <v-text-field v-model="docId.category" label="category" />
-          <v-text-field v-model="docId.name" label="name" />
-          <v-divider />
-          <v-text-field v-model="doc.title" label="title" />
-          <v-text-field v-model="doc.date" label="date" />
-          <v-text-field v-model="doc.description" label="description" />
-          <v-divider />
-          <v-textarea v-model="content.text" label="text" />
+          <v-row>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="docId.category" label="category" outlined />
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="docId.name" label="name" outlined />
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="doc.date" label="date" outlined />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field v-model="doc.description" label="description" />
+            </v-col>
+            <v-col cols="12">
+              <v-textarea v-model="content.text" label="text" rows="15" outlined />
+            </v-col>     
+          </v-row>
         </v-card-text>
       </v-form>
+    </v-card>
+    <v-card v-else>
+      <v-card-text class="text-center">
+        <v-btn @click="signIn">
+          signIn
+        </v-btn>
+      </v-card-text>
     </v-card>
   </v-container>
 </template>
@@ -43,18 +62,11 @@ export default {
         createAt: new Date(),
         modifiedAt: new Date(),
         text: ''
-      }
+      },
+      user: null
     }
   },
   methods: {
-    initialValue () {
-      this.category = ''
-      this.name = ''
-      this.title = ''
-      this.date = ''
-      this.description = ''
-      this.text = ''
-    },
     async save () { 
       try {
         const id = this.docId.category + '-' + this.docId.name
@@ -66,13 +78,40 @@ export default {
         this.content.modifiedAt = new Date()
         await this.$fireStore.collection('docs').doc(cid).set(this.content)
         await alert('데이터를 정상적으로 등록하였습니다.')
-        location.reload()
+        // location.reload()
       } catch (err) {
         console.log('faile to database register', err);
         alert('등록에 실패하였습니다.')
       }
+    },
+    async signIn () {
+      this.loading = true
+      try {
+        const provider = new this.$fireAuthObj.GoogleAuthProvider()
+        const auth = await this.$fireAuth.signInWithPopup(provider)
+        this.user = auth.user
+        console.log(this.user)
+      } catch (err) {
+        console.log('error', err);
+      } finally {
+        alert(`${this.user.displayName}님 환영합니다.`)
+        this.loading = false
+      }
+    },
+    async signOut () {
+      this.loading = true
+      try {
+        // eslint-disable-next-line no-unused-vars
+        const provider = new this.$fireAuthObj.GoogleAuthProvider()
+        // eslint-disable-next-line no-unused-vars
+        await this.$fireAuth.signOut()
+        this.user = null
+      } catch (err) {
+        console.log('error', err);
+      } finally {
+        this.loading = false
+      }
     }
-    
   }
 }
 </script>
